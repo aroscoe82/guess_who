8fb37e29a9c34257f142daf8caf48e0944d79432
Amanda Roscoe <amanda@Amandas-MBP.buffalo.rr.com>
2019-10-13 14:48:49 -0400

    Updates

---
+++ precache-manifest.0dc880f25412b85e232501c0b230a798.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a74b0daa6a949f118f765c9676a6c8d5",
+    "url": "/aroscoe82/guess_who/index.html"
+  },
+  {
+    "revision": "c840fe1636e2cf061ca3",
+    "url": "/aroscoe82/guess_who/static/css/main.b1e6881e.chunk.css"
+  },
+  {
+    "revision": "888ddf7d1a6c571e5603",
+    "url": "/aroscoe82/guess_who/static/js/2.fb0b7269.chunk.js"
+  },
+  {
+    "revision": "c840fe1636e2cf061ca3",
+    "url": "/aroscoe82/guess_who/static/js/main.79e3ab83.chunk.js"
+  },
+  {
+    "revision": "c24708997f7058059acf",
+    "url": "/aroscoe82/guess_who/static/js/runtime-main.0d822561.js"
+  }
+]);
